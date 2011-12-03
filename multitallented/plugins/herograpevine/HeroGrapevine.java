b6c62fdce9b474e655be6fd595e414797faa7b80
Multitallented <multitallented@pacbell.net>
2011-12-02 16:51:41 -0800

    added command processing

--- multitallented/plugins/herograpevine/HeroGrapevine.java
+++ multitallented/plugins/herograpevine/HeroGrapevine.java
@@ -1,8 +1,10 @@
 package multitallented.plugins.herograpevine;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
@@ -13,7 +15,9 @@ public class HeroGrapevine extends JavaPlugin {
     private PluginListener pluginListener;
     private EntityDamageListener entityListener;
     private PlayerInteractListener playerListener;
+    protected FileConfiguration config;
     private FakeListener fakeListener;
+    private ArrayList<Player> ignoredPlayers = new ArrayList<Player>();
     private Tip lastTip;
     
     
@@ -28,6 +32,9 @@ public class HeroGrapevine extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        config = getConfig();
+        processConfig();
+        
         pluginListener = new PluginListener();
         
         PluginManager pm = this.getServer().getPluginManager();
@@ -44,6 +51,7 @@ public class HeroGrapevine extends JavaPlugin {
         //TODO use method of callback?
         
         
+        //TODO add ignored players from config file
         
         
         Logger log = Logger.getLogger("Minecraft");
@@ -53,16 +61,38 @@ public class HeroGrapevine extends JavaPlugin {
         
     }
     
+    
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
         if (!(sender instanceof Player))
             return false;
+        Player player = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("herograpevine")) {
-            //TODO handle commands here
-            return true;
-        } else {
-            Tip tip = new Tip((Player) sender, TipType.COMMAND, cmd.getName());
+            if (args.length >= 1 && args[1].equalsIgnoreCase("toggle") && player.hasPermission("herograpevine.toggle")) {
+                if (ignoredPlayers.contains(player)) {
+                    ignoredPlayers.remove(player);
+                    player.sendMessage("[HeroGrapevine] You will now recieve incoming tips.");
+                } else {
+                    ignoredPlayers.add(player);
+                    player.sendMessage("[HeroGrapevine] You will no longer recieve incoming tips.");
+                }
+                return true;
+            }
+        } else if (config.getString("commands","true").equalsIgnoreCase("true")) {
+            String command = cmd.getName();
+            for (String param : args) {
+                command += " " + param;
+            }
+            lastTip = new Tip(player, TipType.COMMAND, command);
         }
         return false;
+    }
+    
+    private void processConfig() {
+        if (config.getString("commands") == null) {
+            config.set("commands", "true");
+        }
+        
+        //TODO add more config options here
     }
 }
